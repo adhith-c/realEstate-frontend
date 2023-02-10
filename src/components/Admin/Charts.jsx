d91fb03825a98d41e3cf33aabe9b165e02d00858
adhith-c <adhithadhi17@gmail.com>
2023-02-10 13:44:46 +0530

    chart data implemented properly

--- src/components/Admin/Charts.jsx
+++ src/components/Admin/Charts.jsx
@@ -16,6 +16,7 @@ import {
 
 function Charts() {
   const [chartData, setChartData] = useState([]);
+  const [newChart, setnewChart] = useState([]);
   const [monthData, setMonthData] = useState([]);
   const [monthVal, setMonthVal] = useState([]);
 
@@ -32,84 +33,52 @@ function Charts() {
           //     }
         )
         .then((data) => {
-          console.log("data:", data.data.data);
+          //   console.log("data:", data.data.data);
           setChartData(data.data.data);
-          let months = [];
-          let values = [];
+          let newChartData = [];
           data?.data?.data.map((month) => {
-            console.log("month mapper", getMonthName(month._id));
-            months.push(getMonthName(month._id));
-            values.push(month.count);
-            setMonthData(months);
-            setMonthVal(values);
+            // console.log("month:", month);
+            newChartData.push({
+              name: getMonthName(month._id),
+              propertyCount: month.count,
+            });
+            setnewChart(newChartData);
           });
+          //   let months = [];
+          //   let values = [];
+          //   data?.data?.data.map((month) => {
+          //      console.log("month mapper", getMonthName(month._id));
+          //     months.push(getMonthName(month._id));
+          //     values.push(month.count);
+          //     setMonthData(months);
+          //     setMonthVal(values);
+          //   });
         });
     } catch (err) {
       toast.error("Something went wrong.Please try again");
     }
   }, []);
-  console.log("month data", monthData);
-
-  //recharts
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-    },
-  ];
 
   return (
     // recharts
     <BarChart
       width={730}
-      height={250}
-      data={data}>
+      height={350}
+      data={newChart}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar
+      {/* <Bar
         dataKey="pv"
         fill="#8884d8"
-      />
+      /> */}
       <Bar
-        dataKey="uv"
+        dataKey="propertyCount"
         fill="#82ca9d"
       />
     </BarChart>
-    // <Bar data={data} />
   );
 }
 
